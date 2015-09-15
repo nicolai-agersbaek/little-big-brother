630e11b86c61069b26f79e453bd4a0a57f64b0c6
nicolai-agersbaek <nicolai.agersbaek@gmail.com>
2015-09-15 13:10:09 +0200

    Added Google Services connectivity code to MapsActivity. Missing error handling for connection and authorization issues.

--- app/src/main/java/dk/au/cs/nicolai/pvc/littlebigbrother/MapsActivity.java
+++ app/src/main/java/dk/au/cs/nicolai/pvc/littlebigbrother/MapsActivity.java
@@ -3,22 +3,38 @@ package dk.au.cs.nicolai.pvc.littlebigbrother;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 
+import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.GoogleApiClient.ConnectionCallbacks;
+import com.google.android.gms.common.api.GoogleApiClient.OnConnectionFailedListener;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsActivity extends FragmentActivity {
+public class MapsActivity extends FragmentActivity
+    implements ConnectionCallbacks, OnConnectionFailedListener{
 
     private GoogleMap mMap; // Might be null if Google Play services APK is not available.
+    private GoogleApiClient mGoogleApiClient;
+
+    // TODO: Save current camera position in Location (?) private field.
+    // TODO: Add list of current map markers.
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
         setUpMapIfNeeded();
+
+        // TODO: Get map camera position from Intent, if provided (from Reminders activities)
+
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApiIfAvailable(LocationServices.API)
+                .build();
     }
 
     @Override
@@ -56,12 +72,52 @@ public class MapsActivity extends FragmentActivity {
     }
 
     /**
-     * This is where we can add markers or lines, add listeners or move the camera. In this case, we
-     * just add a marker near Africa.
-     * <p/>
-     * This should only be called once and when we are sure that {@link #mMap} is not null.
+     * <p>This is where we can add markers or lines, add listeners or move the camera.<p/>
+     *
+     * <p>This should only be called once and when we are sure that {@link #mMap} is not null.</p>
      */
     private void setUpMap() {
-        mMap.addMarker(new MarkerOptions().position(new LatLng(0, 0)).title("Marker"));
+        // TODO: Get previous camera position from state.
+        // TODO: Get map markers from state.
+
+        //mMap.addMarker(new MarkerOptions().position(new LatLng(0, 0)).title("Marker"));
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if (!mResolvingError) {  // more about this later
+            mGoogleApiClient.connect();
+        }
+    }
+
+    @Override
+    protected void onStop() {
+        mGoogleApiClient.disconnect();
+        super.onStop();
+    }
+
+    public void onConnected(Bundle connectionHint) {
+        // Connected to Google Play services!
+        // The good stuff goes here.
+
+        // TODO: Get user location and set map camera position.
+        // TODO: Allow Intent-provided data to override this setting.
+    }
+
+    @Override
+    public void onConnectionSuspended(int cause) {
+        // The connection has been interrupted.
+        // Disable any UI components that depend on Google APIs
+        // until onConnected() is called.
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult result) {
+        // This callback is important for handling errors that
+        // may occur while attempting to connect with Google.
+        //
+        // More about this in the 'Handle Connection Failures' section.
+        ...
     }
 }
